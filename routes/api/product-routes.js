24ff74a6d1eac57ba5875b34ce4503420e1faaa2
JaimeVilches87 <jaimevilches87@gmail.com>
2021-09-06 23:12:10 -0400

    added category routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,8 +5,21 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [{
+        model: Category
+      },
+      {
+        model: Tag,
+        as: 'productTag'
+      }
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // get one product
